a860b9d79b34c88481518f6ba5324f8f07bdfb82
Jay Nagpaul <jaynagpaul@users.noreply.github.com>
2023-02-16 18:37:27 -0500

    initial version of nvim config

---
+++ lua/jaynagpaul/init.lua
@@ -0,0 +1,2 @@
+require("jaynagpaul.remap")
+require("jaynagpaul.lazy")
---
+++ lua/jaynagpaul/lazy.lua
@@ -0,0 +1,44 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- PLUGINS
+plugins = {
+	{"savq/melange-nvim", lazy = false, priority = 1000},
+
+	{"nvim-telescope/telescope.nvim", tag = '0.1.1',
+		dependencies = {"nvim-lua/plenary.nvim",}},
+
+	{"nvim-treesitter/nvim-treesitter", build=":TSUpdate"},
+
+	{"VonHeikemen/lsp-zero.nvim", branch = "v1.x",
+		dependencies = {
+			-- LSP Support
+			{"neovim/nvim-lspconfig"},             -- Required
+			{"williamboman/mason.nvim"},           -- Optional
+			{"williamboman/mason-lspconfig.nvim"}, -- Optional
+
+			-- Autocompletion
+			{"hrsh7th/nvim-cmp"},         -- Required
+			{"hrsh7th/cmp-nvim-lsp"},     -- Required
+			{"hrsh7th/cmp-buffer"},       -- Optional
+			{"hrsh7th/cmp-path"},         -- Optional
+			{"saadparwaiz1/cmp_luasnip"}, -- Optional
+			{"hrsh7th/cmp-nvim-lua"},     -- Optional
+
+			-- Snippets
+			{"L3MON4D3/LuaSnip"},             -- Required
+			{"rafamadriz/friendly-snippets"}, -- Optional
+		}},
+}
+
+require("lazy").setup(plugins)
