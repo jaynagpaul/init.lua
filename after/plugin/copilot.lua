a4f4b582d17b05a87940548e3cd3fa008102ec7b
Jay Nagpaul <jaynagpaul@users.noreply.github.com>
2023-03-02 14:30:47 -0500

    update deps and add copilot

---
+++ after/plugin/copilot.lua
@@ -0,0 +1,6 @@
+vim.g.copilot_assume_mapped = true
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-u>", 'copilot#Accept("<CR>")', {
+	silent = true,
+	expr = true
+})
--- lua/jaynagpaul/set.lua
+++ lua/jaynagpaul/set.lua
@@ -1,9 +1,10 @@
 local vim = vim
 
--- FOLD Settings
+-- Fold Settings
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldlevel = 99
 
+-- Number Settings
 vim.wo.number = true
 vim.wo.relativenumber = true
